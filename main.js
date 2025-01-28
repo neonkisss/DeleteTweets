8e79e6c8bc9bb6cb3cdd8b2af8930f67f9a78f3c
neonkisss <elesnowxx@gmail.com>
2025-01-28 11:56:30 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-var authorization = "Bearer ***"; // replace by authorization value
+var authorization = "Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA"; // replace by authorization value
 var ua = navigator.userAgentData.brands.map(brand => `"${brand.brand}";v="${brand.version}"`).join(', ');
-var client_tid = "***"; // replace by X-Client-Transaction-Id value
-var client_uuid = "***"; // replace by X-Client-Uuid value
+var client_tid = "FGdgRHaHdHWwRJTvxDI+jK52Lth+Lzod4qS4E5g5wuhB1k18oqaVk+HmfSMDOgfN+0xFXRdFyy5v12xgRNlFQpBUQI3DFw"; // replace by X-Client-Transaction-Id value
+var client_uuid = "02364e4d-0db6-41fd-a042-7ca3efba251a"; // replace by X-Client-Uuid value
 var csrf_token = getCookie("ct0");
 var random_resource = "uYU5M2i12UhDvDTzN6hZPg";
 var random_resource_old_tweets = "H8OOoI-5ZE4NxgRr8lfyWg"
@@ -60,8 +60,8 @@ var delete_options = {
 		Let's say you want to delete tweets from past 6 months. Today is September 19th 2023.
 		You would set after_date to 2023-03-18 (effectively 6 months ago) and before_date 2023-09-20 (tomorrow's date. So it deletes tweets from today too) 
 	*/
-	"after_date":new Date('1900-01-01'), // year-month-day
-	"before_date":new Date('2100-01-01') // year-month-day
+	"after_date":new Date('2020-01-01'), // year-month-day
+	"before_date":new Date('2024-03-01') // year-month-day
 }
 
 function buildAcceptLanguageString() {
